60b736f90efe925f6cd71b12ef27bb4449e1f4a4
llewelyn-williams <llewelyn.williams@googlemail.com>
2021-08-05 21:26:33 +0000

    feat: Attach remaining sample sounds to the board
    You can now click on all the tiles and produce Sample Sounds 1 - 6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,36 @@
-document.getElementById("card1").addEventListener("click", playSound);
+document.getElementById("card1").addEventListener("click", playSound1);
+document.getElementById("card2").addEventListener("click", playSound2);
+document.getElementById("card3").addEventListener("click", playSound3);
+document.getElementById("card4").addEventListener("click", playSound4);
+document.getElementById("card5").addEventListener("click", playSound5);
+document.getElementById("card6").addEventListener("click", playSound6);
 
-function playSound(){
+function playSound1(){
     let audio = new Audio("assets/sounds/sound1.ogg");
     audio.play();
-    console.log("Hello from click function");
+}
+
+function playSound2(){
+    let audio = new Audio("assets/sounds/sound2.ogg");
+    audio.play();
+}
+
+function playSound3(){
+    let audio = new Audio("assets/sounds/sound3.ogg");
+    audio.play();
+}
+
+function playSound4(){
+    let audio = new Audio("assets/sounds/sound4.ogg");
+    audio.play();
+}
+
+function playSound5(){
+    let audio = new Audio("assets/sounds/sound5.ogg");
+    audio.play();
+}
+
+function playSound6(){
+    let audio = new Audio("assets/sounds/sound6.ogg");
+    audio.play();
 }
